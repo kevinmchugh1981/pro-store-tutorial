3d042b44079df0a88471e919b5d459af8d90cedb
Kevin <kevin.mchugh81@googlemail.com>
2025-06-09 17:27:21 +0100

    Commit to change branch.

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -4,7 +4,7 @@ import { Cart } from "@/types";
 import { useToast } from "@/hooks/use-toast";
 import { useTransition } from "react";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
-import { Loader, Minus, Plus } from "lucide-react";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import {
@@ -16,10 +16,14 @@ import {
   TableCell,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/lib/utils";
+import { CardContent, Card } from "@/components/ui/card";
+import { useRouter } from "next/navigation";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const { toast } = useToast();
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   return (
     <>
@@ -105,12 +109,27 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
                         )}
                       </Button>
                     </TableCell>
-                    <TableCell className="text-right">${item.price}</TableCell>
+                    <TableCell className="text-right">{formatCurrency(item.price)}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </div>
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <div className="pb-4 text-xl">
+                Subtotal ({cart.items.reduce((a,c)=> a + c.qty,0 )}):
+                <span className="font-bold">{formatCurrency(cart.itemsPrice)}</span>
+              </div>
+              <Button className="w-full" disabled={isPending} onClick={()=> startTransition(()=> router.push('/shipping-address'))}>
+                {isPending ? (
+                  <Loader className="w-4 h-4 animate-spin" />
+                ) : (
+                  <ArrowRight className="w-3 h-4" />
+                )} Proceed to Checkout
+                </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
--- components/shared/header/product/product-price.tsx
+++ components/shared/header/product/product-price.tsx
@@ -5,7 +5,7 @@ const ProductPrice = ({value, className}: {value:number, className?:string}) => 
   const [intValue, floatValue] =  stringValue.split('.');
   
     return ( <p className={cn('text-2xl', className)}>
-        <span className="text-xs align-super">$</span>
+        <span className="text-xs align-super">£</span>
         {intValue}
         <span className="text-xs align-super">.{floatValue}</span>
     </p> );
