3d5fd681b2f1bf7d845570376440b8fd2772de19
Kevin <kevin.mchugh81@googlemail.com>
2025-06-26 16:33:23 +0100

    Final change to fix bug.

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -16,7 +16,7 @@ export const paypal = {
         purchase_units: [
           {
             amount: {
-              currency_code: "GBP",
+              currency_code: "USD",
               value: price,
             },
           },
