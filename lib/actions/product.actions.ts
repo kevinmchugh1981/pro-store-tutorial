5e2a2d1abfe488c35c9d391bec2e2266e6e01ff8
Kevin <kevin.mchugh81@googlemail.com>
2025-07-21 09:58:27 +0100

    Added "Products" to admin.

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,26 +1,66 @@
-'use server'
+"use server";
 import { prisma } from "@/db/prisma";
-import { convertToPlainObject } from "../utils";
-import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { convertToPlainObject, formatError } from "../utils";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
+import { revalidatePath } from "next/cache";
 
 //Get Latest Products
 
-export async function getLatestProducts()
-{
-    const data = await prisma.product.findMany({
-        take: LATEST_PRODUCTS_LIMIT,
-        orderBy: {createdAt:'desc'}
-    });
+export async function getLatestProducts() {
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: "desc" },
+  });
 
-    return convertToPlainObject(data);
+  return convertToPlainObject(data);
 }
 
 //Get single product by its slug
-export async function getProductBySlug(slug: string)
-{
-    return await prisma.product.findFirst({
-        where: {slug: slug}
-    });
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
 
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
 
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
+
+//Delete a product
+export async function deleteProduct(productId: string) {
+  try {
+    const productExists = await prisma.product.findFirst({
+      where: { id: productId },
+    });
+
+    if (!productExists) throw new Error("Product not found");
+
+    await prisma.product.delete({ where: { id: productId } });
+
+    revalidatePath("/admin/products");
+    return { success: true, message: "Product deleted successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
