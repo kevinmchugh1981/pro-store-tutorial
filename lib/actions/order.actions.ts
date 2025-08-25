f3d91c5863036a91d1a194ec9ccb904db17734ab
Kevin <kevin.mchugh81@googlemail.com>
2025-08-25 09:41:22 +0100

    Added "Hook" for paid by stripe.

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -203,7 +203,7 @@ export async function approvePaypalOrder(
 }
 
 //Update order to paid.
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
